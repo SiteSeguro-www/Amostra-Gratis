259ec3038dd904777f2eadd4857b9641444d3614
DevDW <dweminem@gmail.com>
2025-11-26 07:03:47 -0300

    feat: Add Monetag popup ads
    Integrates a new Monetag popup ad script to enhance ad visibility. Also updates the service worker to use the correct zone ID for the popup ad.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10238511
+    "zoneId": 10238532
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
